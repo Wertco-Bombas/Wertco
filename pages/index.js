68e89e8243d18c08145c47c3bf68c17be468f04f
Wertco-Bombas <suporte2@wertco.com.br>
2026-05-15 16:59:25 -0300

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,1 +1,10 @@
-
+export default function Home() {
+  return (
+    <div>
+      <h1>Minha Base de Conhecimento</h1>
+      <p>Bem-vindo ao projeto colaborativo!</p>
+      <link rel="stylesheet" href="/style.css" />
+      <script src="/script.js"></script>
+    </div>
+  );
+}
