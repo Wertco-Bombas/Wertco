e21cd71c1e923038bff6e4ba5a91a3f0db811d55
Wertco-Bombas <suporte2@wertco.com.br>
2026-05-16 12:14:01 -0300

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,6 @@
 import '../styles/globals.css'
 import '../styles/style.css'
 
-export default function MyApp({ Component, pageProps }) {
+export default function App({ Component, pageProps }) {
   return <Component {...pageProps} />
 }
