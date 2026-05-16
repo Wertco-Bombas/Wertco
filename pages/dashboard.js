ee2f0f2a73fbc70eccd989b608e205150a105f23
Wertco-Bombas <suporte2@wertco.com.br>
2026-05-16 10:47:26 -0300

    Update dashboard.js

--- pages/dashboard.js
+++ pages/dashboard.js
@@ -1,8 +1,25 @@
 export default function Dashboard() {
   return (
     <div className="dashboard-container">
-      <h1>Bem-vindo ao Dashboard!</h1>
-      <p>Você está logado e pode acessar o conteúdo protegido.</p>
+      <h1>Bem-vindo!</h1>
+      <p>Escolha para onde deseja ir:</p>
+      <div className="dashboard-buttons">
+        <button onClick={() => (window.location.href = "/base")}>
+          Base de conhecimento
+        </button>
+        <button onClick={() => (window.location.href = "/usuario")}>
+          Usuário
+        </button>
+        <button onClick={() => (window.location.href = "/treinamento")}>
+          Treinamento
+        </button>
+        <button onClick={() => (window.location.href = "/auditoria")}>
+          Auditoria
+        </button>
+        <button onClick={() => (window.location.href = "/atendimento")}>
+          Atendimento
+        </button>
+      </div>
     </div>
   );
 }
