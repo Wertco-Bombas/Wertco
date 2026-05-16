183b34c524b5bb1b7b707470a90f2419cfa444ff
Wertco-Bombas <suporte2@wertco.com.br>
2026-05-16 08:16:39 -0300

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,5 @@
-import '../styles/style.css'
+import '../styles/globals.css'
+import '../public/style.css'   // importa seu CSS customizado
 
 export default function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
