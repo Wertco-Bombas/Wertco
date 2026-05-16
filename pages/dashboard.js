5a0b30405ee10a636964ed7429c34e0523d654a3
Wertco-Bombas <suporte2@wertco.com.br>
2026-05-16 08:17:26 -0300

    Update dashboard.js

--- pages/dashboard.js
+++ pages/dashboard.js
@@ -1,6 +1,6 @@
 export default function Dashboard() {
   return (
-    <div className="container">
+    <div className="dashboard-container">
       <h1>Bem-vindo ao Dashboard!</h1>
       <p>Você está logado e pode acessar o conteúdo protegido.</p>
     </div>
