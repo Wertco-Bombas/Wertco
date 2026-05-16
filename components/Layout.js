7bd957b6faa010090b99bdb4b439f50877ad5bb5
Wertco-Bombas <suporte2@wertco.com.br>
2026-05-16 12:14:19 -0300

    Update Layout.js

--- components/Layout.js
+++ components/Layout.js
@@ -1,24 +1,9 @@
 export default function Layout({ children }) {
-  function handleLogout() {
-    window.location.href = "/login";
-  }
-
-  function goMenu() {
-    window.location.href = "/dashboard";
-  }
-
-  // Detecta se está na tela de Menu (dashboard)
-  const isMenuPage = window.location.pathname === "/dashboard";
-
   return (
-    <div className="layout-container">
-      <nav className="menu">
-        <div className="menu-right">
-          {!isMenuPage && <button onClick={goMenu}>Menu</button>}
-          <button onClick={handleLogout}>Sair</button>
-        </div>
-      </nav>
-      <main>{children}</main>
+    <div className="page">
+      <div className="container">
+        {children}
+      </div>
     </div>
-  );
+  )
 }
