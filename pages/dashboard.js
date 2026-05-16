54a245895871f2c7e55234e56f7453f3b7efb6aa
Wertco-Bombas <suporte2@wertco.com.br>
2026-05-16 12:04:42 -0300

    Update dashboard.js

--- pages/dashboard.js
+++ pages/dashboard.js
@@ -3,9 +3,31 @@ import Layout from "../components/Layout";
 export default function Dashboard() {
   return (
     <Layout>
-      <div className="dashboard-container">
-        <h1>Bem-vindo</h1>
-        <p>Use o Menu acima para navegar pelas páginas.</p>
+      <div className="menu-container">
+        <h1>Menu</h1>
+        <p>Escolha uma das opções abaixo:</p>
+
+        <section className="menu-options">
+          <div className="card" onClick={() => (window.location.href = "/base")}>
+            <h2>Base de conhecimento</h2>
+          </div>
+
+          <div className="card" onClick={() => (window.location.href = "/usuario")}>
+            <h2>Usuários</h2>
+          </div>
+
+          <div className="card" onClick={() => (window.location.href = "/treinamento")}>
+            <h2>Treinamento</h2>
+          </div>
+
+          <div className="card" onClick={() => (window.location.href = "/auditoria")}>
+            <h2>Auditoria</h2>
+          </div>
+
+          <div className="card" onClick={() => (window.location.href = "/atendimento")}>
+            <h2>Atendimento</h2>
+          </div>
+        </section>
       </div>
     </Layout>
   );
