08bf7d6dedf8bc9a20b8d48254f314572fec714b
Wertco-Bombas <suporte2@wertco.com.br>
2026-05-16 11:39:51 -0300

    Create atendimento.js

---
+++ atendimento.js
@@ -0,0 +1,12 @@
+import Layout from "../components/Layout";
+
+export default function Atendimento() {
+  return (
+    <Layout>
+      <div className="page-container">
+        <h1>Atendimento</h1>
+        <p>Suporte e acompanhamento de chamados.</p>
+      </div>
+    </Layout>
+  );
+}
