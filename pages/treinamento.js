7a9dad382a99e01b46f67e349a4b8f63f3349edc
Wertco-Bombas <suporte2@wertco.com.br>
2026-05-18 12:12:20 -0300

    Create treinamento.js

---
+++ pages/treinamento.js
@@ -0,0 +1,16 @@
+export default function Treinamento() {
+
+  return (
+    <div className="page">
+      <div className="container">
+
+        <h1>Treinamento</h1>
+
+        <button onClick={() => history.back()}>
+          Voltar
+        </button>
+
+      </div>
+    </div>
+  );
+}
