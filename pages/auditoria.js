e47b5f59660ae29a235d12ee472e241615a9f63d
Wertco-Bombas <suporte2@wertco.com.br>
2026-05-18 12:12:44 -0300

    Create auditoria.js

---
+++ pages/auditoria.js
@@ -0,0 +1,16 @@
+export default function Auditoria() {
+
+  return (
+    <div className="page">
+      <div className="container">
+
+        <h1>Auditoria</h1>
+
+        <button onClick={() => history.back()}>
+          Voltar
+        </button>
+
+      </div>
+    </div>
+  );
+}
