e936a10c9a11b20eeb55c99d11384e801fee20c1
Wertco-Bombas <suporte2@wertco.com.br>
2026-05-19 08:24:16 -0300

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,7 @@
 import '../styles/globals.css'
 import '../styles/globals.css';
 import '../styles/base.css';   // aqui você importa o tema escuro/amarelo
+import '../styles/style.css'   // ou '../styles/globals.css' conforme seu projeto
 
 
 export default function App({ Component, pageProps }) {
