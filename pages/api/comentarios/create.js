12bc9f8583f046cb190e493266e08ed9e84fc115
Wertco-Bombas <suporte2@wertco.com.br>
2026-05-19 15:33:09 -0300

    Update create.js

--- pages/api/comentarios/create.js
+++ pages/api/comentarios/create.js
@@ -1,5 +1,7 @@
-await supabase.from('comentarios').insert({
-  conteudo,
-  topico_id,
-  usuario_id: user.id // 👈 usa o nome correto da coluna
-});
+await supabase
+  .from('comentarios')
+  .insert({
+    conteudo,
+    topico_id,
+    usuario_id: user.id // 👈 usa o nome correto
+  });
