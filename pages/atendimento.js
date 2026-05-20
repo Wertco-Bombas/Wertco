632183d0a19228902a0c1fe52162d73f61038d08
Wertco-Bombas <suporte2@wertco.com.br>
2026-05-20 15:35:13 -0300

    Create atendimento.js

---
+++ pages/atendimento.js
@@ -0,0 +1,11 @@
+export default function Atendimento() {
+  return (
+    <div>
+      <h1>Atendimento</h1>
+
+      <p>
+        Aqui ficará o fluxo guiado de suporte para o atendente.
+      </p>
+    </div>
+  );
+}
