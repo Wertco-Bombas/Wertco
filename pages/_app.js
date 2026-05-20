caa30607177df34595abb0af844fab8b83b16acc
Wertco-Bombas <suporte2@wertco.com.br>
2026-05-20 08:49:29 -0300

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -3,9 +3,21 @@ import '../styles/globals.css';
 import '../styles/style.css';
 import '../styles/base.css';
 import Layout from '../components/Layout';
+import { useRouter } from 'next/router';
 
 export default function App({ Component, pageProps }) {
-  return (
+  const router = useRouter();
+
+  // Rotas que não devem usar o Layout
+  const noLayoutRoutes = ['/login', '/signup'];
+
+  const isNoLayout = noLayoutRoutes.includes(router.pathname);
+
+  return isNoLayout ? (
+    // Renderiza sem Layout (login e signup)
+    <Component {...pageProps} />
+  ) : (
+    // Renderiza com Layout (páginas internas)
     <Layout>
       <Component {...pageProps} />
     </Layout>
